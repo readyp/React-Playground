3fc39d40e97799d22ec5e307f971060be7a081f3
readyp <readypamungkas@gmail.com>
2023-02-04 16:59:21 +0700

    tailwindcss starter init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-6xl">Tailwindcss Starter</h1>
+    </div>
+  );
 }
 
 export default App;
